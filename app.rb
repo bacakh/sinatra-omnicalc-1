f9f9eda76f85c8738239fc038d3d5a4f8d40fd84
bacakh <bacakh0@chicagobooth.edu>
2025-02-06 23:06:46 +0000

    94p commit

--- app.rb
+++ app.rb
@@ -1,14 +1,17 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/howdy") do
-erb(:hello)
-end
-
-get("/goodbye") do
-  erb(:bye)
+get("/square_root/new") do
+  erb(:new_sq_root_calc)
   end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 0.5
   
+  erb(:sq_root_results)
+  end
 
   get("/square/new") do
     erb(:new_square_calc)
@@ -22,9 +25,38 @@ get("/goodbye") do
       erb(:square_results)
       end
 
+
+      get("/payment/new") do
+        erb(:new_pay)
+        end
+    
+        get("/payment/results") do
+          @apr = params.fetch("users_apr").to_f
+          @year = params.fetch("users_yr").to_f
+          @pri = params.fetch("users_p").to_f
+    
+          @r = @apr/1200
+          @n = @year * 12
+          @payment = (@pri* @r)/(1-((1+@r)**(-@n)))
+          
+          erb(:payment_results)
+          end
+          
+          get("/random/new") do
+            erb(:rand_calc)
+            end
+        
+            get("/random/results") do
+              @minu = params.fetch("users_min").to_f
+              @maxi = params.fetch("users_max").to_f
+        
+              @the_random = rand(@minu..@maxi)
+              
+              erb(:rand_results)
+              end         
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
+  <p>Enter a number</p>
+
   "
 end
